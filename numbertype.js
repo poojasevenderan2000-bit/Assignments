fa4e2d8113bc6ff2cca9c8d438151f1ecec4d4e5
poojasevenderan2000-bit <poojasevenderan2000@gmail.com>
2026-05-01 10:37:11 +0530

    Add files via upload

---
+++ numbertype.js
@@ -0,0 +1,16 @@
+function numberType(num){
+
+    if (num > 0){
+        console.log("The number",num,"is positive");
+    }
+
+    else if (num <0){
+        console.log("The number",num,"is negative");
+    }
+
+    else{
+        console.log("The number",num,"is Zero"); 
+    }
+}
+
+numberType(1);
